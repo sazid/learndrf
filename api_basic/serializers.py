96b62b20f4b905bc8cbde0793ba433939046fda4
sazid <sazidozon@gmail.com>
2021-05-31 09:46:49 +0600

    use `api_view` to auto render a web api doc/interface

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -5,4 +5,5 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['id', 'title', 'email', 'author']
+        # fields = ['id', 'title', 'email', 'author']
+        fields = '__all__' # get all fields
