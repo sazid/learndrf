802ebf85a59e031f1f1909af2eadbd0ce719e334
sazid <sazidozon@gmail.com>
2021-05-31 10:50:43 +0600

    generic api view

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import article_list, article_detail, ArticleAPIView, ArticleDetails
+from .views import article_list, article_detail # normal function based views
+from .views import ArticleAPIView, ArticleDetails # APIView
+from .views import GenericAPIView # generic views
 
 urlpatterns = [
     # path('article/', article_list),
     # path('detail/<int:pk>/', article_detail),
+
     path('article/', ArticleAPIView.as_view()),
     path('detail/<int:pk>/', ArticleDetails.as_view()),
+
+    # notice that we're routing to the same view, but one which have no pk
+    # another which have pk
+    path('generic/article/', GenericAPIView.as_view()),
+    path('generic/article/<int:pk>/', GenericAPIView.as_view()),
 ]
--- api_basic/views.py
+++ api_basic/views.py
@@ -1,12 +1,44 @@
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, HttpRequest
-from rest_framework import status
+from rest_framework import status, generics, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.views import APIView
 from .models import Article
 from .serializers import ArticleSerializer
+
+
+class GenericAPIView(
+    generics.GenericAPIView,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+    lookup_field = 'pk'
+
+
+    def get(self, request: Request, pk=None):
+        if pk:
+            return self.retrieve(request)
+
+        return self.list(request)
+
+
+    def post(self, request: Request, pk=None):
+        return self.create(request)
+
+
+    def put(self, request: Request, pk=None):
+        return self.update(request)
+
+
+    def delete(self, request: Request, pk=None):
+        return self.destroy(request)
 
 
 class ArticleAPIView(APIView):
